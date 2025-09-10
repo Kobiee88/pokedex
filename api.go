6c3154933dd85f61bde6ae6bdcb1bd6efef52622
Kobiee88 <thomas.mysliwietz@gmail.com>
2025-09-10 22:10:40 +0200

    C: Finished project

--- api.go
+++ api.go
@@ -70,6 +70,89 @@ type LocationArea struct {
 	} `json:"pokemon_encounters"`
 }
 
+type Pokemon struct {
+	ID              int    `json:"id"`
+	Name            string `json:"name"`
+	Base_Experience int    `json:"base_experience"`
+	Height          int    `json:"height"`
+	Is_Default      bool   `json:"is_default"`
+	Order           int    `json:"order"`
+	Weight          int    `json:"weight"`
+	Abilities       []struct {
+		Is_Hidden bool `json:"is_hidden"`
+		Slot      int  `json:"slot"`
+		Ability   struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"ability"`
+	} `json:"abilities"`
+	Forms []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"forms"`
+	Game_Indices []struct {
+		Game_Index int `json:"game_index"`
+		Version    struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"version"`
+	} `json:"game_indices"`
+	Held_Items               []interface{} `json:"held_items"`
+	Location_Area_Encounters string        `json:"location_area_encounters"`
+	Moves                    []struct {
+		Move struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"move"`
+		Version_Group_Details []struct {
+			Level_Learned_At int `json:"level_learned_at"`
+			Version_Group    struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version_group"`
+			Move_Learn_Method struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"move_learn_method"`
+		} `json:"version_group_details"`
+	} `json:"moves"`
+	Past_Types     []interface{} `json:"past_types"`
+	Past_Abilities []interface{} `json:"past_abilities"`
+	Species        struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"species"`
+	Sprites struct {
+		Back_Default       string `json:"back_default"`
+		Back_Shiny         string `json:"back_shiny"`
+		Front_Default      string `json:"front_default"`
+		Front_Shiny        string `json:"front_shiny"`
+		Front_Female       string `json:"front_female"`
+		Front_Shiny_Female string `json:"front_shiny_female"`
+		Back_Female        string `json:"back_female"`
+		Back_Shiny_Female  string `json:"back_shiny_female"`
+	} `json:"sprites"`
+	Cries struct {
+		Latest string `json:"latest"`
+		Legacy string `json:"legacy"`
+	} `json:"cries"`
+	Stats []struct {
+		Base_Stat int `json:"base_stat"`
+		Effort    int `json:"effort"`
+		Stat      struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Slot int `json:"slot"`
+		Type struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"type"`
+	} `json:"types"`
+}
+
 var cache *pokecache.Cache
 
 func initCache() {
@@ -139,4 +222,31 @@ func printPokemonOfLocationArea(name string) {
 	for _, poke := range locationArea.PokemonEncounters {
 		fmt.Println(poke.Pokemon.Name)
 	}
+}
+
+func getPokemon(name string) (Pokemon, bool) {
+	fullUrl := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", name)
+	pokemon := Pokemon{}
+	res, err := http.Get(fullUrl)
+	if err != nil {
+		fmt.Printf("%s is not a valid Pokemon name\n", name)
+		return pokemon, false
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		fmt.Printf("%s is not a valid Pokemon name\n", name)
+		return pokemon, false
+	}
+	if err != nil {
+		fmt.Printf("%s is not a valid Pokemon name\n", name)
+		return pokemon, false
+	}
+	cache.Add(fullUrl, body)
+	err = json.Unmarshal(body, &pokemon)
+	if err != nil {
+		fmt.Printf("ERROR: %s", err)
+		return pokemon, false
+	}
+	return pokemon, true
 }
--- command.go
+++ command.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"os"
 )
 
@@ -12,6 +14,8 @@ type cliCommand struct {
 }
 
 var commands = map[string]cliCommand{}
+
+var pokedex = map[string]Pokemon{}
 
 var currentOffset = 0
 
@@ -62,6 +66,61 @@ func commandExplore(name string) error {
 	return nil
 }
 
+func commandCatch(name string) error {
+	fmt.Printf("Throwing a Pokeball at %s...\n", name)
+	if currentPokemon, ok := getPokemon(name); !ok {
+		return nil
+	} else {
+		if rand.Float64() > math.Min(0.9, math.Max(0.3, 0.1+float64(currentPokemon.Base_Experience)/1000)) {
+			fmt.Printf("%s escaped!\n", currentPokemon.Name)
+			return nil
+		}
+		pokedex[currentPokemon.Name] = currentPokemon
+		fmt.Printf("%s was caught!\n", currentPokemon.Name)
+		return nil
+	}
+}
+
+func commandInspect(name string) error {
+	if currentPokemon, ok := pokedex[name]; !ok {
+		fmt.Printf("you have not caught %s yet\n", name)
+		return nil
+	} else {
+		printPokemonDetails(currentPokemon)
+		return nil
+	}
+}
+
+func commandPokedex(name string) error {
+	if len(pokedex) == 0 {
+		fmt.Println("you have not caught any Pokemon yet")
+		return nil
+	}
+	fmt.Println("You have caught the following Pokemon:")
+	for _, p := range pokedex {
+		fmt.Printf("- %s\n", p.Name)
+	}
+	return nil
+}
+
+func printPokemonDetails(pokemon Pokemon) {
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Weight: %d\n", pokemon.Weight)
+	fmt.Printf("Types: ")
+	for i, t := range pokemon.Types {
+		if i > 0 {
+			fmt.Printf(", ")
+		}
+		fmt.Printf("%s", t.Type.Name)
+	}
+	fmt.Printf("\n")
+	fmt.Printf("Stats:\n")
+	for _, s := range pokemon.Stats {
+		fmt.Printf("  %s: %d\n", s.Stat.Name, s.Base_Stat)
+	}
+}
+
 func initCommandList() {
 	commands["exit"] = cliCommand{
 		name:        "exit",
@@ -87,5 +146,20 @@ func initCommandList() {
 		name:        "explore",
 		description: "Explore a location - provide the location name as an argument",
 		callback:    commandExplore,
+	}
+	commands["catch"] = cliCommand{
+		name:        "catch",
+		description: "Catch a Pokemon - provide the Pokemon name as an argument",
+		callback:    commandCatch,
+	}
+	commands["inspect"] = cliCommand{
+		name:        "inspect",
+		description: "Inspect a caught Pokemon - provide the Pokemon name as an argument",
+		callback:    commandInspect,
+	}
+	commands["pokedex"] = cliCommand{
+		name:        "pokedex",
+		description: "List all caught Pokemon",
+		callback:    commandPokedex,
 	}
 }
